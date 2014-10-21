e96d3b0b2368d8b92788d63052518130242816b2
ayoncin <ayon@didei.mx>
2014-10-21 00:38:17 -0500

    a

--- index.php
+++ index.php
@@ -1,42 +1,9 @@
 <?php
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 if($Ayon = "chivista")
-	echo "Es puto";
-$RobersuiT="Chingon";
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/master
-$CHIVAS="Sergio";
+	echo $RobersuiT="Chingon";
+
 $ayon="chivas";
 
 $RobersuiT="PUTO";
 
-<<<<<<< HEAD
-=======
-=======
-
-<<<<<<< HEAD
-	RobersuiT
-=======
-FAVIAN
-
->>>>>>> origin/master
-=======
->>>>>>> origin/master
-=======
->>>>>>> b70d5ea2945b68b5346b435102e16cff2b217aa9
-<<<<<<< HEAD
->>>>>>> aaeec686afe81bb66a1ce5c32510671639b005cf
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> FETCH_HEAD
->>>>>>> Stashed changes
->>>>>>> e7ea4782070e6367b5d656e16f2bba535e3c5336
->>>>>>> origin/master
->>>>>>> origin/master
 ?>
