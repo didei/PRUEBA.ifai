acdb45eb198998c15be7ced79bb8ec86be196b9d
ayoncin <ayon@didei.mx>
2014-10-21 00:36:59 -0500

    a

--- index.php
+++ index.php
@@ -1,36 +1,7 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
 $CHIVAS="Sergio";
-=======
 $ayon="chivas";
-	RobersuiT
-=======
-FAVIAN
 
->>>>>>> origin/master
-=======
 $RobersuiT="PUTO";
 
-=======
-
-<<<<<<< HEAD
-	RobersuiT
-=======
-FAVIAN
-
->>>>>>> origin/master
-=======
->>>>>>> origin/master
-=======
->>>>>>> b70d5ea2945b68b5346b435102e16cff2b217aa9
-<<<<<<< HEAD
->>>>>>> aaeec686afe81bb66a1ce5c32510671639b005cf
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> FETCH_HEAD
->>>>>>> Stashed changes
->>>>>>> e7ea4782070e6367b5d656e16f2bba535e3c5336
 ?>
