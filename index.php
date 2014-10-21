df933cf09d76feb04587314e6d58adf331abf1a8
ayoncin <ayon@didei.mx>
2014-10-21 00:20:13 -0500

    a

--- index.php
+++ index.php
@@ -1,38 +1,4 @@
 <?php
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-$var="ARRIBA EL AMÉRICA POR QUE ES EL MEJOR EQUIPO DE FUTBOL DEL MUNDO"; //RobersuiT
-=======
-$var="ARRIBA EL AMÉRICA     HELLO WORLD(FAVIAN) ";
->>>>>>> origin/master
-
-$var="ARRIBA LAS CHIVAS";
-$abajo="al hoyo";
-$chingon = "Ayón";
-
-echo $var." y La chivas se vaaaaannnn al ".$abajo;
-echo "el ayon no sabe de futbol"; //RobersuiT
-
-echo "Aqui estuvó ".$chingon;  //El chivista mas chingon
-
-echo "ESTA ES UNA NUEVA LINEA FAVIAN";
-
-=======
->>>>>>> 90311aeed40587300c0276967281d7624547ec23
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-$var="ARRIBA EL MEJOR EQUIPO DEL MUNDO 'CHIVAS'"; //ayon
-=======
-$var="ARRIBA EL AMÉRICA No (Diego)";
->>>>>>> 3cbdeac7462423ca3d6db53fca92416a8b72586a
-=======
-$var="ARRIBA EL AMÉRICA     HELLO WORLD(FAVIAN) ";
->>>>>>> origin/master
-=======
->>>>>>> 5e3dc3de51c7d566bcafd4018bd3b08e46cbb54b
-
-
 ?>
