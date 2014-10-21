d3066de7bc6ebf23813fb84992aa9f0af38677ae
Roberto Omar Rodriguez Guiza <robersuit@hotmail.com>
2014-10-21 00:04:20 -0500

    Cambio
    Cambio Robert

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $abajo="al hoyo";
 $chingon = "Ayón";
 
 echo $var." y La chivas se vaaaaannnn al ".$abajo;
+echo "el ayon no sabe de futbol"; //RobersuiT
 
 echo "Aqui estuvó ".$chingon;  //El chivista mas chingon
 
