5e3dc3de51c7d566bcafd4018bd3b08e46cbb54b
Favian Flores Lira <favian@didei.mx>
2014-10-21 00:16:51 -0500

    clean

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 
 
-<<<<<<< HEAD
-$var="ARRIBA EL AMÉRICA No (Diego)";
-=======
-$var="ARRIBA EL AMÉRICA     HELLO WORLD(FAVIAN) ";
->>>>>>> origin/master
 
-$var="ARRIBA LAS CHIVAS";
-$abajo="al hoyo";
-$chingon = "Ayón";
-
-echo $var." y La chivas se vaaaaannnn al ".$abajo;
-echo "el ayon no sabe de futbol"; //RobersuiT
-
-echo "Aqui estuvó ".$chingon;  //El chivista mas chingon
-
-echo "ESTA ES UNA NUEVA LINEA FAVIAN";
 
 ?>
